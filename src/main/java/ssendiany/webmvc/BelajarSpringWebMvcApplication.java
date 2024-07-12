39f0969b05bdc2b9b08b05f99a3bf5dcf4baafda
SSendiany <septiantosendiany@gmail.com>
2024-07-12 16:01:43 +0700

    init

---
+++ src/main/java/ssendiany/webmvc/BelajarSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package ssendiany.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssendiany/webmvc/BelajarSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ssendiany.webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
